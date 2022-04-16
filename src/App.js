164f6dd2b319768d9b34df10a83e209602aa873a
Carlos Henrique dos Santos <71515378+carloshsanto@users.noreply.github.com>
2022-04-16 15:12:58 -0300

    Estrutura do Site Pronta

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import './App.css'
+
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+// importar componentes criados
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Contact from './Pages/Contact/Contact';
+import NavBarApp from './Layouts/Navbar/Navbar';
+
+
 function App() {
   return (
     <div className="App">
  
-        ola mundo
+        <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<NavBarApp />}>
+                <Route index element={<Home /> } />
+                <Route path='About' element={<About /> } />                
+                <Route path='Contact' element={<Contact /> } />
+
+                <Route path='*' element={ <Navigate replace to="/"/> } />
+            </Route>
+        </Routes>
+        </BrowserRouter>
+        
     </div>
   );
 }
---
+++ src/Layouts/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import { Navbar, Nav, Container } from "react-bootstrap"
+import { Outlet, Link } from "react-router-dom"
+
+
+const NavBarApp = () => {
+    return (
+        <>
+            < Navbar className="navBg" bg="light" expand="lg" >
+                <Container>
+                    <Navbar.Brand as={Link} to="/" ><img
+                        alt=""
+                        //src="/Assets/Img/logo.png"
+                       // width="30"
+                       // height="30"
+                        className="d-inline-block align-top"
+                    />{' '}LCS Blog
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/" >Home</Nav.Link>
+                            <Nav.Link as={Link} to="/About" >Sobre</Nav.Link>
+                            <Nav.Link as={Link} to="/Contact" >Contato</Nav.Link>                            
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        
+
+        <section>
+            <Outlet></Outlet>
+        </section>
+        </>
+    )
+}
+export default NavBarApp
+
+
+
