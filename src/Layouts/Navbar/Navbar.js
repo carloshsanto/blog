f15c0920642255d9e89d24b59dff4c578687fc10
Carlos Henrique dos Santos <71515378+carloshsanto@users.noreply.github.com>
2022-05-02 13:43:29 -0300

    mudanca de cor navbar e pesquisar

--- src/Layouts/Navbar/Navbar.js
+++ src/Layouts/Navbar/Navbar.js
@@ -1,19 +1,20 @@
-import { Navbar, Nav, Container } from "react-bootstrap"
+import { Navbar, Nav, Container,Button, Form ,FormControl  } from "react-bootstrap"
 import { Outlet, Link } from "react-router-dom"
 
 const NavBarApp = () => {
     return (
         <>
-
-            < Navbar className="navBg" bg="light" expand="lg" >
+            < Navbar className="navBg" bg="dark" variant="dark" expand="lg" >
                 <Container>
-                    <Navbar.Brand as={Link} to="/" ><img
-                        alt=""
-                        src="../Img/logo.png"
-                        width="100"
-                        height="40"
-                        className="d-inline-block align-top"
-                    />{' '}
+                    <Navbar.Brand as={Link} to="/" >
+                    <img
+          alt=""
+          src="../Img/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      Blog React
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="container-fluid" />
                     <Navbar.Collapse className="justify-content-end">
@@ -24,8 +25,15 @@ const NavBarApp = () => {
                             <Nav.Link as={Link} to="/Contact" >Contato</Nav.Link>
                             <Nav.Link as={Link} to="/Login" >Login</Nav.Link>
                         </Nav>
-                       
-                       
+                        <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Pesquisar"
+          className="me-2"
+          aria-label="Pesquisar"
+        />
+        <Button variant="outline-light">Pesquisar</Button>
+      </Form>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
